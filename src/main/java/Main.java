b158d0b25450c62604f99e431417aedc6e41cea4
starforce333 <starforce@i.ua>
2019-03-03 21:32:23 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+import java.sql.*;
+
+public class Main {
+    static final String DB_CONNECTION = "jdbc:mysql://localhost:3306/testproc?serverTimezone=Europe/Kiev&useSSL=false";
+    static final String DB_USER = "star";
+    static final String DB_PASSWORD = "password";
+
+    public static void main(String[] args) throws SQLException{
+        try (Connection conn = DriverManager.getConnection(DB_CONNECTION, DB_USER, DB_PASSWORD)) {
+            try (CallableStatement cs = conn.prepareCall("{CALL Test(?, ?, ?)}")) {
+                cs.setInt(1, 20); // in
+                cs.setInt(2, 30); // in
+                cs.registerOutParameter(3, Types.INTEGER); // out
+
+                cs.execute();
+
+                System.out.println(cs.getInt(3));
+            }
+        }
+    }
+}
